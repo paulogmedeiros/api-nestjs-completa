2ed0193eb7902aaf5681ce26a97c976459194438
paulogmedeiros <paulogsantosm@gmail.com>
2024-07-13 21:12:44 -0300

    feat (app.module) configurando as variaveis de ambiente

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // Torna o módulo de configuração disponível globalmente
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
