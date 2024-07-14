3b534d64ebd6b3db78813eddfe3187fc1d2a5ea6
paulogmedeiros <paulogsantosm@gmail.com>
2024-07-13 23:30:54 -0300

    feat (main) configurando o CORS

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  //Configuração do CORS
+  app.enableCors();
+  
   await app.listen(process.env.PORT);
 }
 bootstrap();
